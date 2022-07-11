9c5d3a70160973ca90412c00d00a344a9e6f1b9a
Anibal Corral Serrano <anibal.corral@gmail.com>
2022-07-11 11:13:50 -0400

    Babel and Reactdom installed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('Starting course');
+// console.log('Starting course');
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// ReactDOM.render('This is a React App', document.body)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+   <p>'This is a React App</p>
+);
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,26 @@ module.exports = {
                 filename: 'app.bundle.js'
         },
         plugins: [
-                new HtmlWebpackPlugin()
-        ]
+                new HtmlWebpackPlugin({
+                    template:'src/index.html'
+                })
+        ],
+
+        module: {
+            rules: [
+                    {
+                            test: /\.js$/,
+                            exclude: /node_modules/,
+                            use: {
+                                    loader: 'babel-loader',
+                                    options: {
+                                            presets: [
+                                                    '@babel/preset-env',
+                                                    '@babel/preset-react'
+                                            ]
+                                    }
+                            }
+                    }
+            ]
+    }  
 }
