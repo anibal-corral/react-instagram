cc64323ffb9e7e9d8504eba4384a8b49ddbe481b
Anibal Corral Serrano <anibal.corral@gmail.com>
2022-07-11 12:25:35 -0400

    Category componente styled with css in js technique

---
+++ src/components/App/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Category } from '../Category/Category'
+function App () {
+  return (
+    <Category />
+  )
+}
+
+export { App }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 // console.log('Starting course');
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
+import { App } from './components/App/App'
 // ReactDOM.render('This is a React App', document.body)
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <p>'This is a React App</p>
+  <App />
 )
