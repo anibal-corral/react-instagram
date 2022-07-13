5fdb10ddc3e949376a8803a485f0e26254627d66
Anibal Corral Serrano <anibal.corral@gmail.com>
2022-07-13 10:50:15 -0400

    Add svg logo

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { CategoryList } from '../CategoryList/CategoryList'
 import { GlobalStyle } from '../../GlobalStyles'
 import { PhotoCardList } from '../PhotoCardList/PhotoCardList'
+import { Logo } from '../Logo/Logo'
 function App () {
   return (
     <>
       <GlobalStyle />
+      <Logo />
       <CategoryList />
       <PhotoCardList />
     </>
