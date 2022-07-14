a2b618fbd5aeaa7272c0752a99ff81422486fc1c
Anibal Corral Serrano <anibal.corral@gmail.com>
2022-07-14 09:39:06 -0400

    Using intersection object

--- src/components/PhotoCard/PhotoCard.js
+++ src/components/PhotoCard/PhotoCard.js
@@ -1,20 +1,40 @@
-import React from 'react'
-import { ImgWrapper, Img, Button } from './styles'
+import React, { Fragment, useEffect, useRef, useState } from 'react'
+import { ImgWrapper, Img, Button, Article } from './styles'
 import { FcLike, FcLikePlaceholder } from 'react-icons/fc'
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 function PhotoCard ({ id, likes = 0, src = DEFAULT_IMAGE }) {
+  const [show, setShow] = useState(false)
+  const ref = useRef(null)
+  useEffect(() => {
+    // console.log(ref.current)
+    const observer = new window.IntersectionObserver((entries) => {
+      // console.log(entries)
+      const { isIntersecting } = entries[0]
+      console.log(isIntersecting)
+      if (isIntersecting) {
+        setShow(true)
+        observer.disconnect()
+      }
+    })
+    observer.observe(ref.current)
+  }, [ref])
   return (
-    <article>
-      <a href={`/detail/${id}`}>
-        <ImgWrapper>
-          <Img src={src} />
-        </ImgWrapper>
+    <Article ref={ref}>
+      {
+        show && <>
+          <a href={`/detail/${id}`}>
+            <ImgWrapper>
+              <Img src={src} />
+            </ImgWrapper>
 
-      </a>
-      <Button>
-        <FcLikePlaceholder size='32px' />{likes} likes!
-      </Button>
-    </article>
+          </a>
+          <Button>
+            <FcLikePlaceholder size='32px' />{likes} likes!
+          </Button>
+        </>
+      }
+
+    </Article>
   )
 }
 export { PhotoCard }
--- src/components/PhotoCard/styles.js
+++ src/components/PhotoCard/styles.js
@@ -1,6 +1,10 @@
 import styled from 'styled-components'
 import { fadein } from '../../styles/animation'
 
+const Article = styled.article`
+min-height: 200px
+`
+
 const ImgWrapper = styled.div`
   border-radius: 10px;
   display: block;
@@ -30,4 +34,4 @@ const Button = styled.button`
   }
 `
 
-export { ImgWrapper, Img, Button }
+export { ImgWrapper, Img, Button, Article }
