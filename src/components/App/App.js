6cc9cf6c7427c8611c29e4e4634ac4ac84eac1df
Anibal Corral Serrano <anibal.corral@gmail.com>
2022-07-25 15:55:15 -0400

    Nav bar template

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,23 +4,22 @@ import { PhotoCardWithQuery } from '../../containers/PhotoCardWithQuery'
 import { Logo } from '../Logo/Logo'
 import { Home } from '../../pages/Home'
 import { Router } from '@reach/router'
+import { NavBar } from '../NavBar/NavBar'
 
 function App () {
   const urlParams = new window.URLSearchParams(window.location.search)
   const detailId = urlParams.get('detail')
-  console.log(detailId)
   return (
     <>
       <GlobalStyle />
       <Logo />
-      {
-        detailId
-          ? <PhotoCardWithQuery id={detailId} />
-          : <Router>
-            <Home path='/' />
-            <Home path='/pet/:categoryId' />
-            </Router>
-      }
+
+      <PhotoCardWithQuery id={detailId} />
+      <Router>
+        <Home path='/' />
+        <Home path='/pet/:categoryId' />
+      </Router>
+      <NavBar />
 
     </>
 
