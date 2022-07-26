dd9b268dc897b89b3baeeb1198dd0c30a4c07d8a
Anibal Corral Serrano <anibal.corral@gmail.com>
2022-07-26 10:19:53 -0400

    Showing detail with routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,28 @@
 import React from 'react'
 import { GlobalStyle } from '../../styles/GlobalStyles'
-import { PhotoCardWithQuery } from '../../containers/PhotoCardWithQuery'
 import { Logo } from '../Logo/Logo'
 import { Home } from '../../pages/Home'
 import { Router } from '@reach/router'
 import { NavBar } from '../NavBar/NavBar'
+import { Favs } from '../../pages/Favs'
+import { Detail } from '../../pages/Detail'
+import { User } from '../../pages/User'
+import { NotRegisteredUser } from '../../pages/NotRegisteredUser'
 
 function App () {
-  const urlParams = new window.URLSearchParams(window.location.search)
-  const detailId = urlParams.get('detail')
   return (
     <>
       <GlobalStyle />
       <Logo />
 
-      <PhotoCardWithQuery id={detailId} />
       <Router>
         <Home path='/' />
         <Home path='/pet/:categoryId' />
+        <Detail path='/detail/:detailId' />
+        <Favs path='/favs' />
+        <User path='/user' />
+        <NotRegisteredUser path='/not' />
+
       </Router>
       <NavBar />
 
--- src/components/PhotoCard/PhotoCard.js
+++ src/components/PhotoCard/PhotoCard.js
@@ -5,23 +5,24 @@ import { useLocalStorage } from '../../Hooks/useLocalStorage'
 import { useNearScreen } from '../../Hooks/useNearScreen'
 import { FavButton } from '../FavButton/FavButton'
 import { ToggleLikeMutation } from '../../containers/ToggleLikeMutation'
+import { Link } from '@reach/router'
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 function PhotoCard ({ id, likes = 0, src = DEFAULT_IMAGE }) {
+  console.log('ID photocard ', id)
   const key = `like-${id}`
   const [liked, setLiked] = useLocalStorage(key, false)
-
   const [show, element] = useNearScreen()
 
   return (
     <Article ref={element}>
       {
         show && <>
-          <a href={`/?detail=${id}`}>
+          <Link to={`/detail/${id}`}>
             <ImgWrapper>
               <Img src={src} />
             </ImgWrapper>
 
-          </a>
+          </Link>
           <ToggleLikeMutation>
             {
               (toggleLike) => {
