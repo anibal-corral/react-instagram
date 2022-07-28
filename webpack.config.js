45d32d1837df2b5d728d1ff3986a86927b0df934
Anibal Corral Serrano <anibal.corral@gmail.com>
2022-07-28 10:25:21 -0400

    PWA

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
+const { pathToArray } = require('graphql/jsutils/Path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-
+const WebpackPwaManifestPlugin = require('webpack-pwa-manifest')
+const path = require('path')
 module.exports = {
   mode: 'development',
   output: {
@@ -9,6 +11,20 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html'
+    }),
+    new WebpackPwaManifestPlugin({
+      name: 'Petsgram - Your pet pictures app',
+      short_name: 'Petsgram',
+      description: 'You can find photos of pets.',
+      background_color: '#fff',
+      theme_color: '#4CBCD2',
+      icons: [
+        {
+          src: path.resolve('src/assets/icon.png'),
+          sizes: [96, 128, 192, 256, 384, 512]
+        }
+      ]
+
     })
   ],
 
